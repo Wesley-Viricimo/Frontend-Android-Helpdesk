6d6783fd41d2682e616d29fb1ccc2b7ac86a8b3e
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-09-03 17:47:31 -0300

    Refatorando nome das classes e criando pacotes

--- app/src/main/java/com/example/helpdesk/activity/LoginActivity.java
+++ app/src/main/java/com/example/helpdesk/activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.helpdesk;
+package com.example.helpdesk.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.helpdesk.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
     }
 }
