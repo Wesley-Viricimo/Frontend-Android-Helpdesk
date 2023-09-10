42181ce58bc3fc74c7ce2fa7d02d823ce2f025d6
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-09-09 22:54:35 -0300

    Ajustando recycler view de clientes list

--- app/src/main/java/com/example/helpdesk/ui/cliente/ClientesListFragmentUI.java
+++ app/src/main/java/com/example/helpdesk/ui/cliente/ClientesListFragmentUI.java
@@ -3,19 +3,44 @@ package com.example.helpdesk.ui.cliente;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 
 import com.example.helpdesk.R;
 
 public class ClientesListFragmentUI extends Fragment {
+    private Button btnCadastrarCliente;
+    private Button btnBuscarCliente;
+    private SearchView svBuscarCliente;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clientes_list, container, false);
+
+        View clientesListFragment = inflater.inflate(R.layout.fragment_clientes_list, container, false);
+
+        btnCadastrarCliente = clientesListFragment.findViewById(R.id.btnCadastrarCliente);
+        btnBuscarCliente = clientesListFragment.findViewById(R.id.btnBuscar);
+        svBuscarCliente = clientesListFragment.findViewById(R.id.svBuscarCliente);
+        recyclerView = clientesListFragment.findViewById(R.id.rvClientesList);
+        progressBar = clientesListFragment.findViewById(R.id.pbClientesList);
+
+        btnCadastrarCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("teste");
+            }
+        });
+
+        return clientesListFragment;
     }
 }
