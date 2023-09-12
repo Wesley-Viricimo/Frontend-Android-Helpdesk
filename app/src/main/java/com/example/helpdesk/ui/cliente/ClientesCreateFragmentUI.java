fd1c6eb453d4bf33daf9ccb189e5fdbc3c13875a
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-09-11 21:30:34 -0300

    Implementando editor de máscara de cpf

--- app/src/main/java/com/example/helpdesk/ui/cliente/ClientesCreateFragmentUI.java
+++ app/src/main/java/com/example/helpdesk/ui/cliente/ClientesCreateFragmentUI.java
@@ -7,11 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.helpdesk.R;
 
 
 public class ClientesCreateFragmentUI extends Fragment {
+
+    private EditText edtCadCliNome;
+    private EditText edtCadCliCpf;
+    private EditText edtCadCliEmail;
+    private EditText edtCadCliSenha;
+    private Button btnCadCliCadastro;
+    private Button btnCadCliCancelar;
+    private ProgressBar pbCadCli;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
