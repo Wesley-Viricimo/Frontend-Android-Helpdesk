620543417e60a85c2cafa7d9d81d85386345f50c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-09-23 20:40:35 -0300

    Ajustando layout home fragment

--- app/src/main/java/com/example/helpdesk/ui/HomeFragmentUI.java
+++ app/src/main/java/com/example/helpdesk/ui/HomeFragmentUI.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.helpdesk.R;
 
 public class HomeFragmentUI extends Fragment {
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View homeFragment = inflater.inflate(R.layout.fragment_home, container, false);
+
+        imageView = homeFragment.findViewById(R.id.ivHome);
+
+        Glide.with(this)
+                .load(R.drawable.home_image)
+                .into(imageView);
+
+        return homeFragment;
     }
 }
